c857ed6e8cee0132d1fde5c000a51998bd2b2d23
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-28 15:36:23 +0530

    Updates

---
+++ precache-manifest.d7f88b559c1eea52f60d8e7e0e40efa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6be34560c7ef819e839ba12448d5ff4",
+    "url": "/Ipod.git/index.html"
+  },
+  {
+    "revision": "88a4eace5767472e42d5",
+    "url": "/Ipod.git/static/css/main.21cf0998.chunk.css"
+  },
+  {
+    "revision": "327198e54ac9621e4f08",
+    "url": "/Ipod.git/static/js/2.b446eb98.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/Ipod.git/static/js/2.b446eb98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a4eace5767472e42d5",
+    "url": "/Ipod.git/static/js/main.aeac08ad.chunk.js"
+  },
+  {
+    "revision": "6b8b424ea2756c266a34",
+    "url": "/Ipod.git/static/js/runtime-main.3c892467.js"
+  }
+]);
